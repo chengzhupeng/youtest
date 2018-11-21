21b72e9d7c858b838af231a52972940edc251f58
chegnzhupeng <pengcz@126.com>
2018-11-21 02:28:09 -0500

    update for test only

--- enable_vnc.sh
+++ enable_vnc.sh
@@ -3,6 +3,7 @@ if [ ! -e  /usr/lib/systemd/system/vncserver@.service ];then
 fi
 cp /usr/lib/systemd/system/vncserver@.service /usr/lib/systemd/system/vncserver@\:1.service
 sed -i 's#^ExecStart=.*#ExecStart=/usr/sbin/runuser -l root -c "/usr/bin/vncserver %i"#' /usr/lib/systemd/system/vncserver@\:1.service
+#sed -i 's#^ExecStart=.*#ExecStart=/usr/sbin/runuser -l root -c "/usr/bin/vncserver %i"#' /usr/lib/systemd/system/vncserver@\:1.service
 sed -i 's#^PIDFile=.*#PIDFile=/root/.vnc/%H%i.pid#' /usr/lib/systemd/system/vncserver@\:1.service
 (echo passw0rd ;echo passw0rd;echo n )|vncpasswd
 systemctl enable vncserver@\:1.service
